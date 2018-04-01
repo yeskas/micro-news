c17803a729a476a4db1b730c330d7b503a6dbad4
Yeskendir <yd.kassenov@gmail.com>
2018-04-01 21:34:45 +0600

    Add routes for each request to gateway

--- ms-gateway/gateway.py
+++ ms-gateway/gateway.py
@@ -1,6 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+# Landing page with suggested news and user info
 @app.route('/')
 def index():
 	return 'Hello, World! Welcome to micro-news!'
+
+
+# User clicked on news item to read
+# TODO: remove GET method
+@app.route('/item_clicked', methods=['GET', 'POST'])
+def item_clicked():
+	return 'All tags of the news incremented for user'
+
+
+# User removed the news item from feed
+# TODO: remove GET method
+@app.route('/item_removed', methods=['GET', 'POST'])
+def item_removed():
+	return 'All tags of the news decremented for user'
