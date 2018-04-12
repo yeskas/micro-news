fac0c59645f56ea3525133e70f631f820935dc6a
Yeskendir <yd.kassenov@gmail.com>
2018-04-12 06:28:08 +0600

    scrape all semantic-links for all tags

--- ms-tagger/util/scrape_word_links.py
+++ ms-tagger/util/scrape_word_links.py
@@ -1,25 +1,30 @@
+#!/usr/bin/env python
 import requests
-
-all_words = [
-	"Neuroscience",
-	"Javascript",
-	"Literature",
-	"Technology",
-	"Family",
-	"Software"
-]
-
-for word in all_words:
-	word = word.lower()
-	r = requests.get('http://semantic-link.com/related.php', params={'word': word})
-	links = r.json()[:5]
-
-	# Generate some scala code
-	print '"%s"->Array(' % word
-	for link in links:
-		v = link['v'].lower()
-		score = float(link['mi_norm'])
-		print '\tWordLink("%s", %s),' % (v, score)
-	print '),\n'
+import json
 
 
+# This script builds a json object mapping each tag to its related words
+print '{'
+for line in open('all_tags.txt'):
+	# Add the tag itself
+	tag = line[:-1]
+	links = [{'word': tag, 'score': 1.0}]
+
+	# print 'Querying words related to %s' % tag
+	r = requests.get('http://semantic-link.com/related.php', params={'word': tag})
+	raw_links = r.json()
+
+	found_words = []
+	for raw_link in raw_links:
+		word = raw_link['v'].lower()
+		score = float(raw_link['mi_norm'])
+
+		# ignore duplicates
+		if word not in found_words:
+			links.append({
+				'word': word,
+				'score': score
+			})
+
+	print '\t"%s": %s,' % (tag, json.dumps(links))
+print '}'
