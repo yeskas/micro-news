65d4f1b89b9b8fef867b5cd80c92fd33f32163dc
Yeskendir <yd.kassenov@gmail.com>
2018-04-15 02:53:47 +0600

    rec-sys: add stubs for reabbitmq consumers

---
+++ ms-rec-sys/app/consumers/FeedbackConsumer.scala
@@ -0,0 +1,25 @@
+package consumers
+
+
+import scala.collection.mutable
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+
+import play.api.inject.{SimpleModule, _}
+
+import akka.actor.ActorSystem
+import javax.inject.Inject
+
+import com.datastax.driver.core.{Cluster, ResultSet, Row}
+
+
+class FeedbackConsumerModule extends SimpleModule(bind[FeedbackConsumer].toSelf.eagerly())
+
+class FeedbackConsumer @Inject() (actorSystem: ActorSystem) (implicit executionContext: ExecutionContext) {
+
+	// Make it start @ app start
+	actorSystem.scheduler.schedule(initialDelay = 1.second, interval = 2.seconds) {
+		println("This is FEEDBACK CONSUMER")
+	}
+
+}
